2a5b166e9106c89fce37dad382099130fb100bfc
Gabriel Hoffman <gabriel.hoffman@mssm.edu>
2024-11-04 12:24:56 -0500

    Update install.libs.R
    Copy all static libraries to /libs

--- 2.0/pgenlibr/src/install.libs.R
+++ 2.0/pgenlibr/src/install.libs.R
@@ -1,5 +1,5 @@
 files <- Sys.glob(paste0("*", SHLIB_EXT))
-files <- c(files, "libPLINK2.a")
+files <- c(files, dir(pattern="a$"))
 dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 file.copy(files, dest, overwrite = TRUE)
